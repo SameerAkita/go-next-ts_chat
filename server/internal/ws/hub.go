1d19186fdf8969e6bca0b92c33ec7623e931c69e
Sameer <akita.sameer1@gmail.com>
2026-01-04 03:04:33 +0900

    add hub.run

--- server/internal/ws/hub.go
+++ server/internal/ws/hub.go
@@ -7,7 +7,10 @@ type Room struct {
 }
 
 type Hub struct {
-	Rooms map[string]*Room
+	Rooms      map[string]*Room
+	Register   chan *Client
+	Unregister chan *Client
+	Broadcast  chan *Message
 }
 
 func NewHub() *Hub {
@@ -16,3 +19,38 @@ func NewHub() *Hub {
 	}
 }
 
+func (h *Hub) Run() {
+	for {
+		select {
+		case cl := <-h.Register:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				r := h.Rooms[cl.RoomID]
+
+				if _, ok := r.Clients[cl.ID]; !ok {
+					r.Clients[cl.ID] = cl
+				}
+			}
+		case cl := <-h.Unregister:
+			if _, ok := h.Rooms[cl.RoomID]; ok {
+				if _, ok := h.Rooms[cl.RoomID].Clients[cl.ID]; ok {
+					if len(h.Rooms[cl.RoomID].Clients) != 0 {
+						h.Broadcast <- &Message{
+							Content: "user left the chat",
+							RoomID: cl.RoomID,
+							Username: cl.Username,
+						}
+					}
+
+					delete(h.Rooms[cl.RoomID].Clients, cl.ID)
+					close(cl.Message)
+				}
+			}
+		case m := <-h.Broadcast:
+			if _, ok := h.Rooms[m.RoomID]; ok {
+				for _, cl := range h.Rooms[m.RoomID].Clients {
+					cl.Message <- m
+				}
+			}
+		}
+	}
+}
